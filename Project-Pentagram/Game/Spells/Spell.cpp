1f41a609d190af03ca792faf5da3e301431a882e
ArkuDada <39034809+ArkuDada@users.noreply.github.com>
2022-08-23 20:46:58 +0700

    [PP-130] Spell structs, Init and import + new gitgnore

---
+++ Project-Pentagram/Game/Spells/Spell.cpp
@@ -0,0 +1,60 @@
+﻿#pragma once
+
+#include "../../Utilities/TinyXml/tinyxml2.h"
+#include "Spell.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace tinyxml2;
+using namespace std;
+
+const string const SideEffectType::enum_string[] = {
+    "None",
+    "Mark",
+    "Burn"
+};
+
+const string const ChannelEffectType::enum_string[] = {
+    "None",
+    "Wait",
+    "Active"
+};
+
+SpellBook::SpellBook(string path)
+{
+    XMLDocument doc;
+    doc.LoadFile(path.c_str());
+
+    XMLElement* SpellList = doc.FirstChildElement("SpellBook")->FirstChildElement("SpellList");
+    XMLElement* currSpellElement = SpellList->FirstChildElement("Spell");
+    for (int spell_index = 0; spell_index < 9; spell_index++)
+    {
+        Spell currSpell;
+
+        XMLElement* castTimeElement = currSpellElement->FirstChildElement("CastTime");
+        XMLElement* channelTimeElement = currSpellElement->FirstChildElement("ChannelTime");
+        XMLElement* effectTypeElement = currSpellElement->FirstChildElement("SideEffect");
+        XMLElement* channelTypeElement = currSpellElement->FirstChildElement("ChannelEffect");
+        XMLElement* willValueElement = currSpellElement->FirstChildElement("WillValue")->FirstChildElement("Value");
+        XMLElement* effectValueElement = currSpellElement->FirstChildElement("EffectValue")->FirstChildElement("Value");
+
+        currSpell.SetSpellName(currSpellElement->FirstAttribute()->Value());
+        currSpell.SetCastTime(int(castTimeElement->GetText()));
+        currSpell.SetChannelTime(int(channelTimeElement->GetText()));
+        currSpell.SetSideEffectType(SideEffectType::GetEnum(effectTypeElement->GetText()));
+        currSpell.SetChannelEffectType(ChannelEffectType::GetEnum(channelTypeElement->GetText()));
+        for (int array_index = 0; array_index < 6; array_index++)
+        {
+            currSpell.SetWillValue(array_index,int(willValueElement->GetText()));
+            currSpell.SetSideEffectValue(array_index, int(effectValueElement->GetText()));
+
+            willValueElement = willValueElement->NextSiblingElement();
+            effectValueElement = effectValueElement->NextSiblingElement();
+        }
+        
+        AddNewSpell(currSpell);
+
+        currSpellElement = currSpellElement->NextSiblingElement();
+    }
+}
---
+++ Project-Pentagram/Game/Spells/Spell.h
@@ -0,0 +1,115 @@
+﻿#pragma once
+#include <string>
+#include <map>
+#include <vector>
+using namespace std;
+
+class SpellDatabase
+{
+public:
+    map<string,SpellBook> SpellBooks;
+    void AddSpellBook(string filename) {
+        string path = "Spellbooks/"+filename+".xml";
+        SpellBook newBook(path);
+        SpellBooks.emplace(filename, newBook);
+        newBook.PrintBookDetail();
+    }
+};
+
+class SpellBook
+{
+private:
+    Spell m_Spells[9];
+    int m_Count = 0;
+public:
+    SpellBook(std::string path);
+    void AddNewSpell(Spell newSpell) { m_Spells[m_Count++] = newSpell; }
+    Spell GetSpell(int index) { return m_Spells[index]; }
+    void PrintBookDetail() {};
+
+};
+
+class Spell
+{
+private:
+    std::string m_Name = "New Spell";
+    int m_WillValues[6];
+    SideEffectType::Type m_SideEffectType = SideEffectType::None;
+    int m_SideEffectValues[6] = { 0,0,0,0,0,0 };
+    int m_OriginalCastTime = 1;
+    ChannelEffectType::Type m_ChannelEffectType = ChannelEffectType::None;
+    int m_ChannelTime = 0;
+public:
+
+    Spell() :
+        m_Name("New Spell"),
+        m_WillValues{ 0,0,0,0,0,0 },
+        m_SideEffectType(SideEffectType::None),
+        m_SideEffectValues{ 0,0,0,0,0,0 },
+        m_OriginalCastTime(1),
+        m_ChannelEffectType(ChannelEffectType::None),
+        m_ChannelTime(0)
+    {}
+
+    void SetSpellName(std::string name) { m_Name = name; }
+    std::string GetSpellName() { return m_Name; }
+
+    void SetWillValue(int i, int value) { m_WillValues[i] = value; }
+    int GetWillValue(int i) { return m_WillValues[i]; }
+
+    void SetSideEffectType(SideEffectType::Type type) { m_SideEffectType = type; }
+    SideEffectType::Type GetSideEffectType() { return m_SideEffectType; }
+
+    void SetSideEffectValue(int i, int value) { m_SideEffectValues[i] = value; }
+    int GetSideEffectValue(int i) { return m_SideEffectValues[i]; }
+
+    void SetCastTime(int time) { m_OriginalCastTime = time; }
+    int GetCastTime() { return m_OriginalCastTime; }
+
+    void SetChannelEffectType(ChannelEffectType::Type type) { m_ChannelEffectType = type; }
+    ChannelEffectType::Type GetChannelEffectType() { return m_ChannelEffectType; }
+
+    void SetChannelTime(int time) { m_ChannelTime = time; }
+    int GetChannelTime() { return m_ChannelTime; }
+};
+
+class SideEffectType
+{
+private:
+    static const const string enum_string[];
+public:
+    static enum Type
+    {
+        None = 0,
+        Mark,
+        Burn
+    };
+    static SideEffectType::Type GetEnum(string typeString) {
+        for (int i = 0; i < 3; i++)
+        {
+            if (typeString.compare(enum_string[i])) return SideEffectType::Type(i);
+        }
+        return SideEffectType::None;
+    }
+};
+
+class ChannelEffectType
+{
+private:
+    static const const string enum_string[];
+public:
+    static enum Type
+    {
+        None = 0,
+        Wait,
+        Active
+    };
+    static ChannelEffectType::Type GetEnum(string typeString) {
+        for (int i = 0; i < 3; i++)
+        {
+            if (typeString.compare(enum_string[i])) return ChannelEffectType::Type(i);
+        }
+        return ChannelEffectType::None;
+    }
+};
+
