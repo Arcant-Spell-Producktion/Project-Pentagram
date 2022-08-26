27876ad297187be8cf7cbc0e4c3653288f93d3a6
ArkuDada <39034809+ArkuDada@users.noreply.github.com>
2022-08-26 18:02:10 +0700

    [PP-136] Singleton!

---
+++ Project-Pentagram/Utilities/Singleton.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+template <class T>
+class Singleton
+{
+protected:
+    static T* instance;
+public:
+
+    static T* GetInstance()
+    {
+        if (instance == nullptr)
+        {
+            instance = new T();
+        }
+        return instance;
+    }
+
+    void Free() { delete instance; }
+};
+
+template <class T>
+T* Singleton<T>::instance = nullptr;
