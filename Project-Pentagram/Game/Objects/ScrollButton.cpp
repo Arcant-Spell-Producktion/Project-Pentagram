5bac68639acee9895ce649c549ca457cf20130d8
ArkuDada <39034809+ArkuDada@users.noreply.github.com>
2023-01-23 15:51:17 +0700

    [HotFix] Resize scroll

--- Project-Pentagram/Game/Objects/ScrollButton.cpp
+++ Project-Pentagram/Game/Objects/ScrollButton.cpp
@@ -6,8 +6,8 @@ ScrollButton::ScrollButton(std::string text):Button("Scroll-Button_"+text)
     this->color = { 1.0f, 1.0f, 1.0f, 1.0f };
     this->SetTexture("Sprites/UI/Game/ui_game_scroll.png");
 
-    this->SetIsSlicing(false);
-    this->SetSlicingBorderSize(160.0f);
+    this->SetIsSlicing(true);
+    this->SetSlicingBorderSize(200.0f);
     this->SetSlicingBorderMultiplier(0.25f);
     this->SetSlicingType(SlicingType::REPEAT);
 
