679909f27bac59fde42af7223a2f53ba04f5dc5c
NeeTer <siravich12456@gmail.com>
2023-05-01 14:41:45 +0700

    Add WindSpell1,2 & 3 Animation

--- Project-Pentagram/Game/BattleScene/GameObject/SpellObject/Wind/WindSpellObject.cpp
+++ Project-Pentagram/Game/BattleScene/GameObject/SpellObject/Wind/WindSpellObject.cpp
@@ -9,7 +9,11 @@ BaseSpellObject* WindSpellObject::CreateSpellObject(int index, CasterPosition ta
     switch (index)
     {
     case 0:
+        object = new WindSpell1(target);
+        break;
     case 1:
+        object = new WindSpell2(target);
+        break;
     case 2:
     case 3:
     case 4:
@@ -17,7 +21,7 @@ BaseSpellObject* WindSpellObject::CreateSpellObject(int index, CasterPosition ta
     case 6:
     case 7:
     case 8:
-        object = new WindSpell1(target);
+        object = new WindSpell3(target);
         break;
     }
     return object;
@@ -34,13 +38,87 @@ void WindSpell1::Initialize()
     this->scale = { -size * m_SpellTarget, size, 1.0f };
     glm::vec3 startPos = { startX, yPos, 0.0f };
     glm::vec3 endPos = { endX, yPos, 0.0f };
-    float travelTime = 1.5f;
+    float travelTime = 0.5f;
 
     this->position = startPos;
+    this->SetAnimationPlayTime(0.1f);
     this->SetIsAnimationObject(true);
+    this->SetIsAnimationLoop(false);
 
     m_AudioControllerPtr->PlaySFX("Audio/SFX/Gameplay/Spell/Water/sfx_gameplay_water_shooting_generic.wav", 1.0f);
+    QueueWaitTillFrameEvent(true);
+    QueueWaitEvent(0.5f);
+    QueueUpdateFunction(
+        [this](float dt)
+        {
+            this->SetIsAnimationLoop(true);
+            Next();
+        }
+    );
     QueueMoveEvent(startPos, endPos, travelTime);
+
+    QueueHitEvent();
+    QueueDoneEvent();
+}
+
+void WindSpell2::Initialize()
+{
+    std::cout << "Windball::Init\n";
+    float size = 300.0f;
+    float speed = 10.0f;
+    float startX = (CASTER_POSITION_X - 100.0f) * m_SpellTarget; // Assume A shooter
+    float endX = (-CASTER_POSITION_X + size / 4) * m_SpellTarget; // Assume B recieve
+    float yPos = -160.0f;
+    this->scale = { -size * m_SpellTarget, size, 1.0f };
+    glm::vec3 startPos = { startX, yPos, 0.0f };
+    glm::vec3 endPos = { endX, yPos, 0.0f };
+    float travelTime = 0.75f;
+
+    this->position = startPos;
+    this->SetAnimationPlayTime(0.1f);
+    this->SetIsAnimationObject(true);
+    this->SetIsAnimationLoop(true);
+
+    m_AudioControllerPtr->PlaySFX("Audio/SFX/Gameplay/Spell/Water/sfx_gameplay_water_shooting_generic.wav", 1.0f);
+    QueueWaitEvent(0.5f);
+    QueueUpdateFunction(
+        [this](float dt)
+        {
+            this->SetSpriteByIndex(1, 0, true);
+            Next();
+        }
+    );
+    QueueMoveEvent(startPos, endPos, travelTime);
+    QueueUpdateFunction(
+        [this](float dt)
+        {
+            this->SetSpriteByIndex(2, 0, true);
+            Next();
+        }
+    );
+    QueueWaitTillFrameEvent();
+
+    QueueHitEvent();
+    QueueDoneEvent();
+}
+
+void WindSpell3::Initialize()
+{
+    std::cout << "Windball::Init\n";
+    float size = 400.0f;
+    float speed = 10.0f;
+    float yPos = -200.0f;
+    this->scale = { -size * m_SpellTarget, size / 2.0f, 1.0f };
+    float travelTime = 0.75f;
+
+    this->position = { (-CASTER_POSITION_X) * m_SpellTarget, yPos, 0.0f };
+    this->SetAnimationPlayTime(0.15f);
+    this->SetIsAnimationObject(true);
+    this->SetIsAnimationLoop(true);
+
+    m_AudioControllerPtr->PlaySFX("Audio/SFX/Gameplay/Spell/Water/sfx_gameplay_water_shooting_generic.wav", 1.0f);
+    QueueWaitTillFrameEvent(true);
+    QueueWaitEvent(1.25f);
 
     QueueHitEvent();
     QueueDoneEvent();
--- Project-Pentagram/Game/BattleScene/GameObject/SpellObject/Wind/WindSpellObjects.h
+++ Project-Pentagram/Game/BattleScene/GameObject/SpellObject/Wind/WindSpellObjects.h
@@ -11,12 +11,43 @@ public:
         "Windball_Object",
         "Sprites/Spell/Wind/spell_sprite_wind1.png")
     {
+        std::cout << "Spell:: Create WindCutter\n";
+        Initialize();
+        BaseSpellObject::Initialize();
+    };
+};
+
+class WindSpell2 : public BaseSpellObject
+{
+private:
+    void Initialize();
+public:
+    WindSpell2(CasterPosition target) :BaseSpellObject(
+        target,
+        "Windball_Object",
+        "Sprites/Spell/Wind/spell_sprite_wind2.png")
+    {
         std::cout << "Spell:: Create WindBall\n";
         Initialize();
         BaseSpellObject::Initialize();
     };
 };
 
+class WindSpell3 : public BaseSpellObject
+{
+private:
+    void Initialize();
+public:
+    WindSpell3(CasterPosition target) :BaseSpellObject(
+        target,
+        "Windball_Object",
+        "Sprites/Spell/Wind/spell_sprite_wind3.png")
+    {
+        std::cout << "Spell:: Create WindBall\n";
+        Initialize();
+        BaseSpellObject::Initialize();
+    };
+};
 
 class WindSpellObject
 {
