269a6fe845b801c7dd16b161de9840cd5641becd
NeeTer <siravich12456@gmail.com>
2023-05-12 03:09:55 +0700

    [Hot Fix] Add mutex & once_flag

--- Project-Pentagram/Utilities/Singleton.h
+++ Project-Pentagram/Utilities/Singleton.h
@@ -5,18 +5,18 @@
 template <class T>
 class Singleton
 {
-    protected:
-        static T* instance;
-        static std::mutex mutex;
     public:
-
+        Singleton() = default;
+        ~Singleton() = default;
+        Singleton(const Singleton&) = delete;
+        Singleton& operator=(const Singleton&) = delete;
+        
         static T* GetInstance()
         {
-            std::lock_guard<std::mutex> lock(mutex);
-            if (instance == nullptr)
-            {
+            std::call_once(initFlag, []() {
                 instance = new T();
-            }
+                });
+
             return instance;
         }
 
@@ -26,10 +26,19 @@ class Singleton
             delete instance;
             instance = nullptr;
         }
+
+    private:
+
+        static T* instance;
+        static std::once_flag initFlag;
+        static std::mutex mutex;
 };
 
 template <class T>
 T* Singleton<T>::instance = nullptr;
+
+template <class T>
+std::once_flag Singleton<T>::initFlag;
 
 template <class T>
 std::mutex Singleton<T>::mutex;
