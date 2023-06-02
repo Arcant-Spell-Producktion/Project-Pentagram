dc3f33ca66f21c36ff22231ad61797093411a622
ArmedPipachu <89252284+ArmedPipachinno@users.noreply.github.com>
2023-06-02 18:09:47 +0700

    [Hot fix] Update character selection screen character description

--- Project-Pentagram/Game/CharacterSelectScene/CasterInfo.h
+++ Project-Pentagram/Game/CharacterSelectScene/CasterInfo.h
@@ -9,11 +9,11 @@ debuff enemy power and interrupt them.)";
 
 const std::string EarthEffectInfo[2] =
 {
-	R"(Fortified: When a unit with fortified effect take damage 
-	from spell, turn it to 0 and decrease 1 stack.)",
+	R"(Fortified: Unit with Fortified negate damage 
+	from the next Spell (Max stack 3))",
 
-	R"(Pertify: Unit that suffer from pertify will have 1 less 
-	will for each spell.(can't go below 1))"
+	R"(Petrify: Spell of Unit with Petrify have 1 less 
+	Will per stack, during this turn. (Minimum 1) (Max stack 3) )"
 };
 
 // ------------------------- Fire Character Info -------------------------
@@ -23,12 +23,12 @@ and slowly burn damage.)";
 
 const std::string FireEffectInfo[2] = 
 {
-	R"(Marking: When a unit with mark effect take damage 
-	from spell, Double it.)",
+	R"(Marking: Unit with mark take double damage 
+	from the next spell during this turn.)",
 	
-	R"(Burning: Unit with burning will take 10 damage per stack 
-	at the start of the turn.(Stack decrease by 1 after 
-	take damage))"
+	R"(Burning: Unit with Burning take 10 damage per stack 
+	at the start of the turn. (Stack decrease by 1 after 
+	take damage) )"
 };
 
 // ------------------------- Water Character Info -------------------------
@@ -38,11 +38,12 @@ the enemy cast time.)";
 
 const std::string WaterEffectInfo[2] =
 {
-	R"(Overflow: Gain an extra mana during the battle.
-	(Maximum at 5))",
+	R"(Overflow: Unit with Overflow gain 1 extra mana 
+	per stack during this battle (Max stack 5))",
 
-	R"(Freezing: Unit that suffer from Freezing will use 1 more 
-	mana to adjust cast time.(this effect is stackable))"
+	R"(Freezing: Unit with Freezing use 1 more mana per 
+	stack to decrease Cast Time, during this turn.
+	(Max stack 3))"
 };
 
 // ------------------------- Wind Character Info -------------------------
@@ -52,7 +53,11 @@ will the more she become strong in the next turn.)";
 
 const std::string WindEffectInfo[1] =
 {
-	R"(Boost: When a spell wins in a WillCompare sequence
-	gain a stack. On next turn base gain a buff based on
-	how many stacks you have.)"
+	R"(Boost: Gain stack(s) upon winning spell crash.
+	At start of the turn gain buffs based on the stacks. 
+	2+ stack: Reduce cost for decrease Cast Time this turn by 1 
+	5+ stack: Reduce cost for increasing Will this turn  by 1
+	8 stack: Increase all Spell Damage cast this turn by 10.
+	Upon reaching 8 stack, during this turn no stack is gain
+	and the stack is reset at the end of this turn.)"
 };
